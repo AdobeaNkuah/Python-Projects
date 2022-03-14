7dbf89e11b3827db075e5610a736eaf10e948027
Adobea <48498666+AdobeaNkuah@users.noreply.github.com>
2022-03-14 03:26:07 -0400

    Add files via upload

--- Black Scholes Option Pricing Calculator/BlackScholes.py
+++ Black Scholes Option Pricing Calculator/BlackScholes.py
@@ -1,59 +1,62 @@
 """
 Variable definitions:
-	S: current asset stock price
-	K: strike price
-	r: risk-free interest rate
-	t: time to expiration expressed as fraction of year
-	μ, mew: mean
-	σ, sigma: asset volatility (standard deviation of log returns)
-	***N(d1):
-		For call option: future value of stock given stock_price >= strike_price at option expiration
-		For put option: future value of stock given stock_price < strike_price at option expiration
-	***N(d2):
-		For call option: P(stock_price >= strike_price) at option expiration
-		For put option: P(stock_price < strike_price) at option expiration
+ S: current asset stock price
+ K: strike price
+ r: risk-free interest rate
+ t: time to expiration expressed as fraction of year
+ μ: mean
+ σ: asset volatility (standard deviation of log returns)
+ ***N(d1):
+    For call option: future value of stock given stock price >= strike price at option expiration
+    For put option: future value of stock given stock price < strike price at option expiration
+ ***N(d2):
+    For call option: P(stock_price >= strike_price) at option expiration
+    For put option: P(stock_price < strike_price) at option expiration
 """
 import math
 from scipy.stats import norm
 
-val = 0.1
+# initialize variables
+S = 0  # Test ex S = 100
+K = 0  # K = 100
+r = 0  # r = 0.05
+t = 0  # t = 0.5
+μ = 0  # μ = 0
+σ = 0  # σ = 0.3
+
+
 # Prompt user to enter variable values
-while val is not float:
+# Error handling for input data
+while type(S) is not float:
     try:
         S = float(input("Type stock price, S:  "))  # test entered value using typecast to ensure it's a number
-        val = S
+        print(S)
     except ValueError:
-        print("That's not a number")
+        print("Value error, entered value must be a number")
 
-while K is not float:
+while type(K) is not float:
     try:
         K = float(input("Type strike price, K:  "))
     except ValueError:
-        print("That's not a number")
+        print("Value error, entered value must be a number")
 
-while r is not float:
+while type(r) is not float:
     try:
         r = float(input("Type risk-free interest rate (floating point ratio), r:  "))
     except ValueError:
-        print("That's not a number")
+        print("Value error, entered value must be a number")
 
-while σ is not float:
+while type(σ) is not float:
     try:
         σ = float(input("Type asset volatility (floating point ratio), σ:  "))
     except ValueError:
-        print("That's not a number")
+        print("Value error, entered value must be a number")
 
-while t is not float:
+while type(t) is not float:
     try:
         t = float(input("Type time to expiration (fraction of year), t:  "))
     except ValueError:
-        print("That's not a number")
-
-
-# def normal_pdf(x, μ, σ):
-#     numerator = math.exp(-0.5 * math.pow((x - μ)/σ, 2))
-#     denominator = math.sqrt(2 * math.pi) * σ
-#     return numerator / denominator
+        print("Value error, entered value must be a number")
 
 
 # Calculate the normal cumulative distribution
@@ -77,12 +80,17 @@ def d2(d1, t, σ):
 
 
 def call_price(S, K, r, μ, σ, t, PV,  d1, d2):
-    C = S*N(d1, μ, σ) - PV*N(d2, μ, σ)
+    C = S*N(d1, μ, σ) - PV(K, r, t)*N(d2, μ, σ)
     return C
 
 
 def put_price(S, K, r, μ, σ, t, PV,  d1, d2):
-    P = PV*N(-d2, μ, σ) - S*N(-d1, μ, σ)
+    P = PV(K, r, t)*N(-d2, μ, σ) - S*N(-d1, μ, σ)
     return P
 
-#def main():
+
+print("Call Price: " + call_price(S, K, r, μ, σ, t, PV, d1, d2))
+print("Put Price: " + put_price(S, K, r, μ, σ, t, PV,  d1, d2))
+
+
+# def main():
